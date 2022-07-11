af4d16c77b225cb9672ba2d664f53511009e62f8
rahuldixit2612 <100479629+rahuldixit2612@users.noreply.github.com>
2022-07-11 14:51:17 +0530

    Add files via upload

---
+++ python_code.py
@@ -0,0 +1,17 @@
+print("this is assignment 3, Run Python Code using git clone")
+# Python program to swap two variables
+
+x = 5
+y = 10
+
+# To take inputs from the user
+#x = input('Enter value of x: ')
+#y = input('Enter value of y: ')
+
+# create a temporary variable and swap the values
+temp = x
+x = y
+y = temp
+
+print('The value of x after swapping: {}'.format(x))
+print('The value of y after swapping: {}'.format(y))
